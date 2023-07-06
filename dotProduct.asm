4c2753f0307a1b83c74699572d28b90da8430617
Leo C_G <84103303+LcgDL@users.noreply.github.com>
2023-07-06 20:02:50 +0200

    Add files via upload

---
+++ dotProduct.asm
@@ -0,0 +1,60 @@
+#Dot product
+#r0=120
+movi r0,1
+lsli r0,7
+subi r0,8
+
+#Write vector values to memory
+#(1*3)+(2*4)
+movi r1,1
+stw r1,r0,0
+movi r1,2
+stw r1,r0,1
+movi r1,3
+stw r1,r0,2
+movi r1,4
+stw r1,r0,3
+
+#Pointer
+bseti r14,7
+subi r14,8
+
+#Take values from memory for mult.
+ldw r1,r14,0
+ldw r2,r14,2
+call mull
+
+mov r3, r0
+
+ldw r1,r14,1
+ldw r2,r14,3
+call mull
+
+addu r0,r3
+# Result r0=11
+halt
+
+mull:
+movi r0,0
+movi r10,0
+
+while:
+cmpne r2,r10
+bf ende
+
+mov r9,r2
+andi r9,1
+cmpeq r9,r10
+bf else
+lsli r1,1
+lsri r2,1
+br while
+
+else:
+addu r0,r1
+lsli r1,1
+lsri r2,1
+br while
+
+ende:
+jmp r15 

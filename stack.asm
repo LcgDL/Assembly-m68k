80709b77331ede43a12de6a29aff6e2136f4783b
Leo C_G <84103303+LcgDL@users.noreply.github.com>
2023-07-10 20:32:59 +0200

    Update stack.asm

--- stack.asm
+++ stack.asm
@@ -1,50 +1,54 @@
+#Pointer = 65536
 movi r4,0
 subi r14,1
 
+#Help register
 movi r10,0
-
+#Basis - Exponent
 movi r6,3
 movi r7,5
 
-call potenz
+call exponentiation 
 halt
 
-potenz:
+exponentiation:
+#Set stack return address
 subi r14,2
 stw r15,r14
 
+#If-condition
 cmpgt r7,r10
-bf potenz_sonst
+bf potenz_else
 
 subi r7,1
-call potenz
+call exponentiation 
 
 mov r2,r0
 mov r1,r6
 
 movi r0,0
-solange:
+while:
 cmpne r2,r10
-bf potenz_ende
+bf exponentiation_end
 mov r9,r2
 andi r9,1
 
 cmpeq r9,r10
-bf sonst
+bf else
 lsli r1,1
 lsri r2,1
-br solange
+br while
 
-sonst:
+else:
 addu r0,r1
 lsli r1,1
 lsri r2,1
-br solange
+br while
 
-potenz_sonst:
+exponentiation_else:
 movi r0,1
 
-potenz_ende:
+exponentiation_ende:
 ldw r15,r14
 addi r14,2
 jmp r15 

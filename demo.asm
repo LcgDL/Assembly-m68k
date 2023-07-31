dc44b67394504e42b10aa20eae1e38b9a35bc38b
Leo C_G <84103303+LcgDL@users.noreply.github.com>
2023-07-31 20:21:37 +0200

    Update demo.asm

--- demo.asm
+++ demo.asm
@@ -20,7 +20,7 @@ lsli r7, 8
    #r7+2 => 258
 addi r7,2
 movi r6, 14
-   #258-deci = 102-hex (MEM): 0x0102: 0002 000e 0000
+   #258-deci = 102-hex (MEM): 0x0100: 0002 000e 0000
    #(16 bit Group: 8bits-Adresse[258:00] _ 8bits-Value=0e)
 stw r6,r7
 
@@ -29,7 +29,7 @@ movi r10, 1
    #1*2^8 => r10=256
 lsli r10, 8
    #r10=260
-addi r10, 4
+addi r10, 4v
 movi r5, 1
    #1*2^5 => r5=32
 lsli r5, 5
@@ -53,21 +53,28 @@ movi r2, 0
 ldw r3, r0
 
 loop:
-   #?(r3[2]==r1[0])
+   #?(r3[2]==r1[0])   
+    #?(r3[1]==r1[0]) 
 cmpeq r3, r1
 bt end
-
-   #(Pointer)r0=258
-    #258-deci = 102-hex (MEM): 0x0102: ... 000e
+   #(Pointer)r0=[258] => 14 (000e)
+    #258-deci = 102-hex (MEM): 0x0100: 0002 000e 001f
+     #(Pointer)r0=[260]...
 addi r0, 2
-   #r4=14
+   #r4=14 <= r0=[258]
 ldw r4, r0
 subi r3, 1
+   #0+14
+    #14+31
 addu r2, r4
+   #br -14 =-7(commands)*2
 br loop
 
 end:
+   #r4=45
 mov r4, r2
+   #45=101101 (not change: set bit(1) in 2^0=1)
 bseti r2, 0
+   #r2=0
 subu r2, r4
 halt

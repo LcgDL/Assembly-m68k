d39d53b1fee9dd586dc0ec9859933788b4d7e005
Leo C_G <84103303+LcgDL@users.noreply.github.com>
2023-08-01 18:26:00 +0200

    Update callMulti.asm

--- callMulti.asm
+++ callMulti.asm
@@ -1,17 +1,16 @@
-#Calculate: 10 * 12 + 5
+   #Calculate: 10 * 12 + 5
 movi r1,10
 movi r2,12
 movi r3,5
-
 br mul
 
 continue:
 addu r0,r3
-# Result: r0=125
+   # Result: r0=125
 halt
 
 mul:
-#Auxiliary registers
+   #Auxiliary registers
 movi r0,0
 movi r10,0
 
@@ -21,7 +20,7 @@ bf end
 
 mov r9,r2
 andi r9,1
-#if
+   #if
 cmpeq r9,r10
 bf else
 lsli r1,1

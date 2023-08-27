d938730a6a76bcca5d1ae8624c7189203ac9557f
Leo C_G <84103303+LcgDL@users.noreply.github.com>
2023-08-27 22:41:26 +0200

    Update fibo.asm

--- fibo.asm
+++ fibo.asm
@@ -70,10 +70,12 @@ call if
 # temporarily save: cache #from jmp r15: sonst
    # r5=1
     # r5=2
+     #r5=3
 mov r5, r8
 # write top value of r14 back to r8
    #r8=3 # r14-MEM[65519] = 3
     #r8=4
+     ###
 ldw r8, r14, 0
 # Write buffer (r15) to r14
    #r14=65517

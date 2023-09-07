332b59318bcb1b96981f89ad35efbe2330951931
Leo C_G <84103303+LcgDL@users.noreply.github.com>
2023-09-07 20:47:34 +0200

    Update fibo.asm

--- fibo.asm
+++ fibo.asm
@@ -117,11 +117,12 @@ call if
 # r8 temporarily save: cache #from jmp r15
    #r4=1
     #r4=1
-     ###
+     #r4=1
 mov r4, r8
 # Load value in r5
    #r5=1
     #r5=2
+     ###
 ldw r5, r14, 0
    #r14[MEM-65519]=3
     #r14=65523

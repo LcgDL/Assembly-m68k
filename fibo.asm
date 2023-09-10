8da250b9f4e582f85117a45228c3bd496f94ede0
Leo C_G <84103303+LcgDL@users.noreply.github.com>
2023-09-10 21:50:30 +0200

    Update fibo.asm

--- fibo.asm
+++ fibo.asm
@@ -153,10 +153,11 @@ mov r8, r5
 # Result = a+b
    #r8=2
     #r8=3
-     ###
+     #r8=2
 addu r8, r4
    #call:3 (after: (n-1) call if) #r15-Addr -> mov r5, r8 - 34
     #call:5
+    ###
 jmp r15
 
 sonst:
